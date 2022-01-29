53946021d35a58183c68266d6ac82f4cbd03eaf4
Naran Gomes <gom3snaran@gmail.com>
2022-01-29 14:40:46 -0300

    Adicao do aquivo modelo.cs e criacao da classe ApplicationContext.cs

---
+++ e-Commerce/Aulas/Aula1/CasaDoCodigo/ApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿using CasaDoCodigo.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace CasaDoCodigo
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Produto>().HasKey(k => k.Id);
+        }
+    }
+}
