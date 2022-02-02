cfe413b58655b4c73ad6f019c37d2fc4738a6c11
Naran Gomes <gom3snaran@gmail.com>
2022-02-02 16:26:32 -0300

    Criado todo BaseRepository

---
+++ e-Commerce/Aulas/Aula1/CasaDoCodigo/Repositories/PedidoRepository.cs
@@ -0,0 +1,16 @@
+﻿using CasaDoCodigo.Models;
+
+namespace CasaDoCodigo.Repositories
+{
+    public interface IPedidoRepository
+    {
+
+    }
+    public class PedidoRepository : BaseRepository<Pedido>, IPedidoRepository
+    {
+        
+        public PedidoRepository(ApplicationContext contexto) : base(contexto)
+        {
+        }
+    }
+}
