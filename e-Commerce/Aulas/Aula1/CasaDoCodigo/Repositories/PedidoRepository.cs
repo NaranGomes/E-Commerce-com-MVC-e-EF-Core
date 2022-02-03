42d6f7fae35a72d51b7fe38475e14c33fcf0d7b2
Naran Gomes <gom3snaran@gmail.com>
2022-02-03 14:37:31 -0300

    Criado metodo GetPedido e exibido na View do Carrinho

--- e-Commerce/Aulas/Aula1/CasaDoCodigo/Repositories/PedidoRepository.cs
+++ e-Commerce/Aulas/Aula1/CasaDoCodigo/Repositories/PedidoRepository.cs
@@ -1,11 +1,12 @@
 ﻿using CasaDoCodigo.Models;
 using Microsoft.AspNetCore.Http;
+using System.Linq;
 
 namespace CasaDoCodigo.Repositories
 {
     public interface IPedidoRepository
     {
-
+        Pedido GetPedido();
     }
     public class PedidoRepository : BaseRepository<Pedido>, IPedidoRepository
     {
@@ -13,6 +14,21 @@ namespace CasaDoCodigo.Repositories
         public PedidoRepository(ApplicationContext contexto,
             IHttpContextAccessor contextAccessor) : base(contexto)
         {
+        }
+
+        public Pedido GetPedido()
+        {
+            var pedidoId = GetPedidoId();
+            var pedido = dbSet
+                .Where(p => p.Id == pedidoId)
+                .SingleOrDefault();
+            if (pedido == null)
+            {
+                pedido = new Pedido();
+                dbSet.Add(pedido);
+                contexto.SaveChanges();
+            }
+            return pedido;
         }
 
         private int? GetPedidoId()
